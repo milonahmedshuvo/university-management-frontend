64583597c9f1d9810af85f0f5298298873dfb2d5
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-01-04 01:17:18 +0600

    Dynamic sidebar

--- src/components/ui/SlideBar.tsx
+++ src/components/ui/SlideBar.tsx
@@ -1,48 +1,13 @@
 "use client";
 import { USER_ROLE } from '@/contents/role';
 import sliderItems from '@/contents/sliderItems';
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-
-
-
-import type { MenuProps } from 'antd';
 import {  Menu } from 'antd';
 import { Layout } from "antd";
 import { useState } from "react";
 
 const { Sider } = Layout;
-type MenuItem = Required<MenuProps>['items'][number];
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
 
-const items: MenuItem[] = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
-];
+
 
 
 
@@ -53,7 +18,7 @@ const items: MenuItem[] = [
 const SideBar = () => {
   const [collapsed, setCollapsed] = useState(false);
 
-  const role = USER_ROLE.STUDENT
+  const role = USER_ROLE.ADMIN
 
 
   return (
